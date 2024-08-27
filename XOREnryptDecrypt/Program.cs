643f62a4ac2233bb97d7a0f54a1fc77d918afc56
Bartek <bartlomiej.kacki@gmail.com>
2024-08-27 19:00:47 +0200

    Add project files.

---
+++ XOREnryptDecrypt/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace XOREnryptDecrypt
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = "ptaki lataja kluczem";
+            var key = "haslo";
+            var test = XorEncryptDecrypt.XorEnryptDecrypt(message, key);
+
+            Console.WriteLine(test);
+        }
+    }
+}
---
+++ XOREnryptDecrypt/XorEncryptDecrypt.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XOREnryptDecrypt
+{
+    public static class XorEncryptDecrypt
+    {
+        public static string Message { get; set; }
+        public static string Key { get; set; }
+        public static string EncryptedMessage { get; private set; }
+
+        public static string BinaryEncryptedMessage => StringToBinary(EncryptedMessage);
+
+        public static string XorEnryptDecrypt(string message, string key)
+        {
+            Message = message;
+            Key = key;
+            char[] _enryptedMessage = new char[Message.Length];
+
+            for(int i=0; i < Message.Length; i++)
+            {
+                _enryptedMessage[i] = (char)(Message[i] ^ Key[i % Key.Length]);
+            }
+
+            EncryptedMessage = new string(_enryptedMessage);
+            return BinaryEncryptedMessage;
+        }
+
+        public static string StringToBinary(string data)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in data.ToCharArray())
+            {
+                sb.Append(Convert.ToString(c, 2).PadLeft(8, '0'));
+            }
+            return InsertSpacesEvery8Chars(sb.ToString());
+        }
+
+        public static string InsertSpacesEvery8Chars(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i > 0 && i % 8 == 0)
+                {
+                    sb.Append(' ');
+                }
+                sb.Append(input[i]);
+            }
+            return sb.ToString();
+        }
+    }
+}
